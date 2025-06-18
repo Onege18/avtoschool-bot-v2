e668192565ffe3fdf7f4a3946299f286b37fbedc
Onege18 <Onege18.com>
2025-06-18 10:48:18 +0500

    Fix: поддержка HEAD и GET для /ping

--- telegram_bot.py
+++ telegram_bot.py
@@ -312,19 +312,14 @@ import uvicorn
 api_app = FastAPI()
 telegram_app: Application = None  # объявим здесь глобально
 
+
+
+
 @api_app.api_route("/ping", methods=["GET", "HEAD"])
 def ping():
     return {"status": "ok"}
 
-
-@api_app.head("/ping")
-def ping_head():
-    return {"status": "ok"}
-
 import asyncio
-
-
-
 
 
 
